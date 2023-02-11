af6e5bea52dea1413ed3c05fe126983ddc2e420f
Toni Sevener <tonisevener@gmail.com>
2023-02-11 00:18:45 -0600

    Removing script for now

--- .github/scripts/get-next-build-number.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PREFIX='releases/'
-LASTRELEASETAG=$(git tag -l $PREFIX* --sort=-creatordate | head -n 1)
-LASTBUILD=${LASTRELEASETAG:9}
-LASTBUILDBETTER=${LASTBUILD%??} #temp
-NEXTBUILD=$((LASTBUILDBETTER + 1))
-echo $NEXTBUILD
